177daf15080ce6f5bb807fd64b905f5934f8240d
Connell McCarthy <connell.mccarthy@shopify.com>
2019-11-29 16:07:34 -0500

    RSVP Functions

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,27 @@ $('input').focusout(function(event) {
 		$(this).parent().find('label').removeClass('active');
 	}
 });
+
+$('#rsvp-form').submit(function() {
+	event.preventDefault();
+	var name = encodeURIComponent($('#name').val());
+	var email = encodeURIComponent($('#email').val());
+	var dietary = encodeURIComponent($('#dietary').val());
+	var attend = encodeURIComponent($('#attend').val());
+
+	var entry_name = 'entry.559352220';
+	var entry_email = 'entry.924465480';
+	var entry_dietary = 'entry.521641686';
+	var entry_attend = 'entry.877086558';
+
+	var baseURL = 'https://docs.google.com/forms/d/e/1FAIpQLSckAY7cFO9RQLJM1MnTkj_6qr37NAitFVhgWAKHr1-d3ubT4w/formResponse?';
+	var submitRef = '&submit=Submit';
+	var submitURL = (baseURL + entry_name + '=' + name + '&' + entry_email + '=' + email + '&' + entry_dietary + '=' + dietary + '&' + entry_attend + '=' + attend + submitRef);
+
+	console.log(submitURL);
+	$('#no-target').attr('src', submitURL);
+
+	$('#rsvp-form').fadeOut();
+	$('#rsvp-sub').text('Thank you for RSVP\'ing. Make sure you save the date!');
+	$('.confirmation').fadeIn().css('display', 'inline-block');
+});
