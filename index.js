eb9bf6b1ab748ff04c1dea1acf4bbb28f69d9dbe
Connell McCarthy <connellmccarthy@gmail.com>
2020-03-02 09:26:59 -0500

    CM update

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 var express = require("express");
 var app = express();
 
-var settings = {
-	names: 'Rachel Lin & Alex Johnstone',
-	date: 'Saturday, September 5th, 2020',
-	time: '5:00PM — Late'
-}
-
 app.set("view engine", "jade");
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-	res.render("index", {
-		settings: settings
-	});
+	res.render("index", {});
 });
 
 var server = app.listen(5000, function() {
