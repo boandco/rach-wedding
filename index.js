c47a34169c634ed8ef642b61ed020ea9b3cca882
Connell McCarthy <connellmccarthy@gmail.com>
2022-04-19 14:18:33 -0400

    Update date and deploy settings

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var port = 8888;
 
 app.set("view engine", "pug");
 app.use(express.static('public'));
@@ -8,6 +9,6 @@ app.get('/', function (req, res) {
 	res.render("index", {});
 });
 
-var server = app.listen(5000, function() {
-	console.log('Node server is running..');
+var server = app.listen(port, function() {
+	console.log(`Node server is running at http://localhost:${port}`);
 });
